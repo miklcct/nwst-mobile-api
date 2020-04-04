8eb66d9e2a4d69d847b93e60ab06404e41b41a91
Michael Tsang <miklcct@gmail.com>
2020-04-04 15:25:28 +0800

    add variant identifier

---
+++ src/Model/VariantIdentifier.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace Miklcct\Nwst\Model;
+
+use function explode;
+use function filter_var;
+use function implode;
+use const FILTER_VALIDATE_INT;
+
+class VariantIdentifier {
+    public function __construct(
+        string $company
+        , Rdv $rdv
+        , int $from
+        , int $to
+        , int $id
+        , string $bound
+    ) {
+        $this->company = $company;
+        $this->rdv = $rdv;
+        $this->from_stop = $from;
+        $this->to_stop = $to;
+        $this->id = $id;
+        $this->bound = $bound;
+    }
+
+    public static function parse(string $variant, string $delimiter) : self {
+        $segments = explode($delimiter, $variant);
+        $segments[1] = Rdv::parse($segments[1]);
+        foreach ([2, 3, 4] as $index) {
+            $segments[$index] = filter_var($segments[$index], FILTER_VALIDATE_INT);
+        }
+        return new self(...$segments);
+    }
+
+    public function __toString() : string {
+        return $this->toString('||');
+    }
+
+    public function toString(string $delimiter) : string {
+        return implode(
+            $delimiter
+            , [
+                $this->company,
+                $this->rdv->__toString(),
+                (string)$this->from_stop,
+                (string)$this->to_stop,
+                (string)$this->id,
+                $this->bound,
+            ]
+        );
+    }
+
+    /**
+     * @var string
+     */
+    public $company;
+    /**
+     * @var Rdv
+     */
+    public $rdv;
+    /**
+     * @var int The starting stop shown in the variant
+     */
+    public $from_stop;
+    /**
+     * @var int The ending stop shown in the variant
+     */
+    public $to_stop;
+    /**
+     * @var int A unique number
+     */
+    public $id;
+    /**
+     * @var string
+     */
+    public $bound;
+}
---
+++ test/Model/VariantIdentifierTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Miklcct\Nwst\Test\Model;
+
+use Miklcct\Nwst\Model\Rdv;
+use Miklcct\Nwst\Model\VariantIdentifier;
+use PHPUnit\Framework\TestCase;
+
+class VariantIdentifierTest extends TestCase {
+    public function testParse() : void {
+        $variant = VariantIdentifier::parse('NWFB***14-GRP-3***1***22***10410***I', '***');
+        self::assertSame('NWFB', $variant->company);
+        self::assertEquals(Rdv::parse('14-GRP-3'), $variant->rdv);
+        self::assertSame(1, $variant->from_stop);
+        self::assertSame(22, $variant->to_stop);
+        self::assertSame(10410, $variant->id);
+        self::assertSame('I', $variant->bound);
+    }
+
+    public function test__toString() : void {
+        $variant = new VariantIdentifier('14', Rdv::parse('14-GRP-3'), 1, 22, 10410, 'I');
+        self::assertSame($variant->toString('||'), $variant->__toString());
+    }
+
+    public function testToString() : void {
+        $variant = new VariantIdentifier('NWFB', Rdv::parse('14-GRP-3'), 1, 22, 10410, 'I');
+        self::assertSame('NWFB||14-GRP-3||1||22||10410||I', $variant->toString('||'));
+    }
+}
